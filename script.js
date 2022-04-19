640d04d59c543c2f541fde76bd84f9b33c04a2dd
nmunir22 <munirnadia22@gmail.com>
2022-04-18 23:03:41 -0500

    Basic Crud

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function readFormData() {
     formData["productCode"] = document.getElementById("productCode").value;
     formData["product"] = document.getElementById("product").value;
     formData["qty"] = document.getElementById("qty").value;
-    formData["qty"] = document.getElementById("qty").value;
+    formData["qty1"] = document.getElementById("qty1").value;
     formData["perPrice"] = document.getElementById("perPrice").value;
     return formData;
 }
@@ -34,7 +34,7 @@ function insertNewRecord(data) {
     cell3 = newRow.insertCell(2);
 		cell3.innerHTML = data.qty;
     cell4 = newRow.insertCell(3);
-		cell4.innerHTML = data.qty;
+		cell4.innerHTML = data.qty1;
     cell5 = newRow.insertCell(4);
                 cell5.innerHTML = data.perPrice;
     cell6 = newRow.insertCell(5);
@@ -47,13 +47,15 @@ function onEdit(td) {
     document.getElementById("productCode").value = selectedRow.cells[0].innerHTML;
     document.getElementById("product").value = selectedRow.cells[1].innerHTML;
     document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("qty1").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("perPrice").value = selectedRow.cells[4].innerHTML;
 }
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.productCode;
     selectedRow.cells[1].innerHTML = formData.product;
     selectedRow.cells[2].innerHTML = formData.qty;
-    selectedRow.cells[3].innerHTML = formData.perPrice;
+    selectedRow.cells[3].innerHTML = formData.qty1;
+    selectedRow.cells[4].innerHTML = formData.perPrice;
 }
 
 //Delete the data
@@ -69,6 +71,7 @@ function onDelete(td) {
 function resetForm() {
     document.getElementById("productCode").value = '';
     document.getElementById("product").value = '';
+    document.getElementById("qty").value = '';
     document.getElementById("qty").value = '';
     document.getElementById("perPrice").value = '';
     selectedRow = null;
